f564ae4ffd778a05319b0c618f9a726bf5104387
Craig Scheiderer <craig.scheiderer@southerncrossengineering.com>
2017-02-16 20:10:14 -0500

    Added randomized test bank, scoring, number of tries, and associated GUI elements with error handling to Test Mode

--- SpellingGame/TestForm.Designer.cs
+++ SpellingGame/TestForm.Designer.cs
@@ -36,38 +36,47 @@
             this.btnCheckSpelling = new System.Windows.Forms.Button();
             this.btnAudio = new System.Windows.Forms.Button();
             this.lblSentence = new System.Windows.Forms.Label();
+            this.lblWordCount = new System.Windows.Forms.Label();
+            this.btnFeedback = new System.Windows.Forms.Button();
+            this.lblScore = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxWord)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBoxWord
             // 
             this.pictureBoxWord.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBoxWord.Location = new System.Drawing.Point(26, 27);
+            this.pictureBoxWord.Location = new System.Drawing.Point(17, 18);
+            this.pictureBoxWord.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBoxWord.Name = "pictureBoxWord";
-            this.pictureBoxWord.Size = new System.Drawing.Size(509, 219);
+            this.pictureBoxWord.Size = new System.Drawing.Size(340, 143);
             this.pictureBoxWord.TabIndex = 2;
             this.pictureBoxWord.TabStop = false;
+            this.pictureBoxWord.Click += new System.EventHandler(this.pictureBoxWord_Click);
             // 
             // txtSpelling
             // 
-            this.txtSpelling.Location = new System.Drawing.Point(146, 323);
+            this.txtSpelling.Location = new System.Drawing.Point(97, 210);
+            this.txtSpelling.Margin = new System.Windows.Forms.Padding(2);
             this.txtSpelling.Name = "txtSpelling";
-            this.txtSpelling.Size = new System.Drawing.Size(258, 26);
+            this.txtSpelling.Size = new System.Drawing.Size(173, 20);
             this.txtSpelling.TabIndex = 3;
+            this.txtSpelling.TextChanged += new System.EventHandler(this.txtSpelling_TextChanged);
             // 
             // lblSpelling
             // 
-            this.lblSpelling.Location = new System.Drawing.Point(27, 323);
+            this.lblSpelling.Location = new System.Drawing.Point(18, 210);
+            this.lblSpelling.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblSpelling.Name = "lblSpelling";
-            this.lblSpelling.Size = new System.Drawing.Size(113, 26);
+            this.lblSpelling.Size = new System.Drawing.Size(75, 17);
             this.lblSpelling.TabIndex = 4;
             this.lblSpelling.Text = "Enter Spelling:";
             // 
             // btnCheckSpelling
             // 
-            this.btnCheckSpelling.Location = new System.Drawing.Point(146, 366);
+            this.btnCheckSpelling.Location = new System.Drawing.Point(97, 238);
+            this.btnCheckSpelling.Margin = new System.Windows.Forms.Padding(2);
             this.btnCheckSpelling.Name = "btnCheckSpelling";
-            this.btnCheckSpelling.Size = new System.Drawing.Size(158, 40);
+            this.btnCheckSpelling.Size = new System.Drawing.Size(105, 26);
             this.btnCheckSpelling.TabIndex = 5;
             this.btnCheckSpelling.Text = "Check Spelling";
             this.btnCheckSpelling.UseVisualStyleBackColor = true;
@@ -77,9 +86,10 @@
             // 
             this.btnAudio.BackgroundImage = global::SpellingGame.Properties.Resources.audio_icon;
             this.btnAudio.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnAudio.Location = new System.Drawing.Point(551, 27);
+            this.btnAudio.Location = new System.Drawing.Point(367, 18);
+            this.btnAudio.Margin = new System.Windows.Forms.Padding(2);
             this.btnAudio.Name = "btnAudio";
-            this.btnAudio.Size = new System.Drawing.Size(91, 79);
+            this.btnAudio.Size = new System.Drawing.Size(61, 51);
             this.btnAudio.TabIndex = 6;
             this.btnAudio.UseVisualStyleBackColor = true;
             this.btnAudio.Click += new System.EventHandler(this.btnAudio_Click);
@@ -87,25 +97,59 @@
             // lblSentence
             // 
             this.lblSentence.AutoSize = true;
-            this.lblSentence.Location = new System.Drawing.Point(26, 261);
+            this.lblSentence.Location = new System.Drawing.Point(17, 170);
+            this.lblSentence.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblSentence.Name = "lblSentence";
-            this.lblSentence.Size = new System.Drawing.Size(153, 20);
+            this.lblSentence.Size = new System.Drawing.Size(103, 13);
             this.lblSentence.TabIndex = 7;
             this.lblSentence.Text = "Sentence goes here";
+            this.lblSentence.Click += new System.EventHandler(this.lblSentence_Click);
+            // 
+            // lblWordCount
+            // 
+            this.lblWordCount.AutoSize = true;
+            this.lblWordCount.Location = new System.Drawing.Point(372, 89);
+            this.lblWordCount.Name = "lblWordCount";
+            this.lblWordCount.Size = new System.Drawing.Size(0, 13);
+            this.lblWordCount.TabIndex = 8;
+            // 
+            // btnFeedback
+            // 
+            this.btnFeedback.Location = new System.Drawing.Point(319, 219);
+            this.btnFeedback.Name = "btnFeedback";
+            this.btnFeedback.Size = new System.Drawing.Size(108, 44);
+            this.btnFeedback.TabIndex = 9;
+            this.btnFeedback.Text = "Review Missed Items";
+            this.btnFeedback.UseVisualStyleBackColor = true;
+            this.btnFeedback.Click += new System.EventHandler(this.btnFeedback_Click);
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(337, 184);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(35, 13);
+            this.lblScore.TabIndex = 10;
+            this.lblScore.Text = "lblScore";
             // 
             // TestForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(666, 433);
+            this.ClientSize = new System.Drawing.Size(444, 281);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.btnFeedback);
+            this.Controls.Add(this.lblWordCount);
             this.Controls.Add(this.lblSentence);
             this.Controls.Add(this.btnAudio);
             this.Controls.Add(this.btnCheckSpelling);
             this.Controls.Add(this.lblSpelling);
             this.Controls.Add(this.txtSpelling);
             this.Controls.Add(this.pictureBoxWord);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "TestForm";
             this.Text = "Test in progress! - Time goes here maybe";
+            this.Load += new System.EventHandler(this.TestForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxWord)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -120,5 +164,8 @@
         private System.Windows.Forms.Button btnCheckSpelling;
         private System.Windows.Forms.Button btnAudio;
         private System.Windows.Forms.Label lblSentence;
+        private System.Windows.Forms.Label lblWordCount;
+        private System.Windows.Forms.Button btnFeedback;
+        private System.Windows.Forms.Label lblScore;
     }
 }
--- SpellingGame/TestForm.cs
+++ SpellingGame/TestForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Speech.Synthesis;
 
 namespace SpellingGame
 {
@@ -15,12 +16,128 @@ namespace SpellingGame
         public TestForm()
         {
             InitializeComponent();
-        }       
+            TestWordsForm_Load();
+        }
+
+        private Data.Word word;
+        private int bankFillCount = 0;
+        private int score = 0;
+        private int testWordIndex = 4;
+        private static int numberOfTries = 3;
+        private List<long> wordIdentList = new List<long>();
+        private List<Data.Word> incorrectWords = new List<Data.Word>();
+        private List<Data.Word> testWordBank = new List<Data.Word>();
+
+        private void TestWordsForm_Load()
+        {
+            // Connect to database
+            using (var db = new Data.Database())
+            {
+                while(bankFillCount < 5){
+                    // Get 5 words randomly chosen form the entire list
+                    Random rnd = new Random();
+                    long wordIdent = rnd.Next(562);
+                    if (wordIdentList.Contains(wordIdent)){
+                        continue;
+                    }
+                    wordIdentList.Add(wordIdent);
+                    testWordBank.Add(db.Words.Find(wordIdent));
+                    bankFillCount++;
+                }
+            }
+            txtSpelling.Enabled = false;
+            btnCheckSpelling.Enabled = false;
+            btnFeedback.Enabled = false;
+            lblSentence.Text = "Click on the play audio button to start the test.";
+            lblScore.Text = "Score: " + score;
+        }
+
+        private void loadTestWord()
+        {
+            // Cycle through each word chosen from the word bank. testWordIndex is initialized to 0
+            if (testWordIndex >= 0)
+            {
+                word = testWordBank[testWordIndex];
+                lblWordCount.Text = "Words left:" + testWordIndex;
+                // Show picture
+                if (word.Image != null)
+                {
+                    using (System.IO.MemoryStream ms = new System.IO.MemoryStream(word.Image))
+                    {
+                        pictureBoxWord.Image = Image.FromStream(ms);
+                    }
+                }
+                else
+                {
+                    // Hides the previous picture
+                    pictureBoxWord.Image = null;
+                }
+                // Show sentence
+                lblSentence.ForeColor = Color.Black;
+                lblSentence.Text = word.Sentence;
+            }
+
+            if (testWordIndex < 0)
+            {
+
+                lblSentence.Text = "You have finished the test. Click on Review Missed Items.";
+                btnFeedback.Enabled = true;
+                btnAudio.Enabled = false;
+                txtSpelling.Enabled = false;
+            }
+        }
 
         private void btnCheckSpelling_Click(object sender, EventArgs e)
         {
+            Boolean spellCheckResult;
 
+            if (testWordIndex >= 0)
+            {
+                if (word != null)
+                {
+                   
+                    if (!txtSpelling.Text.Equals(""))
+                    {
+
+                        spellCheckResult = spellCheck(txtSpelling.Text, word.Word1);
+
+                        if (spellCheckResult == false && numberOfTries > 0)
+                        {
+                            lblSentence.ForeColor = Color.Red;
+                            lblSentence.Text = "Incorrect!  Number of tries left: " + (numberOfTries - 1);
+                            incorrectWords.Add(word);
+                            numberOfTries--;
+                        }
+                        if (spellCheckResult == false && numberOfTries == 0)
+                        {
+                            lblSentence.Text = "That was your last chance!  Click on the audio button for the next word";
+                            testWordIndex--;
+                            btnCheckSpelling.Enabled = false;
+                        }
+                        if (spellCheckResult == true)
+                        {
+                            lblSentence.ForeColor = Color.Green;
+                            lblSentence.Text = "Correct!  Click on the audio button for the next word";
+                            testWordIndex--;
+                            score++;
+                            lblScore.Text = "Score: " + score;
+                            numberOfTries = 3;
+                        }
+                    }
+                }
+
+                if (word == null)
+                {
+                    lblSentence.Text = "Press the play audio button to load a new word.";
+                }
+
+                if (txtSpelling.Text.Equals(""))
+                {
+                    lblSentence.Text = "You did not type an answer.  At least try!";
+                }
+            }
         }
+
         private Boolean spellCheck(String s1, String s2)
         {
             return s1.Equals(s2);
@@ -28,12 +145,48 @@ namespace SpellingGame
 
         private void btnAudio_Click(object sender, EventArgs e)
         {
+            txtSpelling.Enabled = true;
+            btnCheckSpelling.Enabled = true;
+            txtSpelling.Text = "";
+            loadTestWord();
             playAudioForCurrentWord();
         }
 
         private void playAudioForCurrentWord()
         {
+            // Speaks the word
+            SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+            synthesizer.Speak("The word is: " + word.Word1);
+        }
+
+        private void lblSentence_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TestForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBoxWord_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtSpelling_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void wordCountdown()
+        {
             
+        }
+
+        private void btnFeedback_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.MessageBox.Show("test");
         }
     }
 }
