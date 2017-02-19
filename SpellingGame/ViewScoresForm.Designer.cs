0c3b0f2353824528744e4b1ec48fff600ce84c48
Preston <cadepres00@gmail.com>
2017-02-19 08:17:18 -0500

    Displays numerical rank (1-10) on gray background.

--- SpellingGame/ViewScoresForm.Designer.cs
+++ SpellingGame/ViewScoresForm.Designer.cs
@@ -28,25 +28,145 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.scores = new System.Windows.Forms.Label();
+            this.s0 = new System.Windows.Forms.Label();
+            this.s1 = new System.Windows.Forms.Label();
+            this.s2 = new System.Windows.Forms.Label();
+            this.s3 = new System.Windows.Forms.Label();
+            this.s4 = new System.Windows.Forms.Label();
+            this.s5 = new System.Windows.Forms.Label();
+            this.s6 = new System.Windows.Forms.Label();
+            this.s7 = new System.Windows.Forms.Label();
+            this.s8 = new System.Windows.Forms.Label();
+            this.s9 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // label1
+            // scores
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(110, 38);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(65, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Scores";
+            this.scores.AutoSize = true;
+            this.scores.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scores.Location = new System.Drawing.Point(110, 18);
+            this.scores.Name = "scores";
+            this.scores.Size = new System.Drawing.Size(65, 20);
+            this.scores.TabIndex = 0;
+            this.scores.Text = "High Scores";
+            // 
+            // s0
+            // 
+            this.s0.AutoSize = true;
+            this.s0.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.s0.Location = new System.Drawing.Point(100, 108);
+            this.s0.Name = "s0";
+            this.s0.Size = new System.Drawing.Size(65, 20);
+            this.s0.TabIndex = 1;
+            this.s0.Text = "1";
+            // 
+            // s1
+            // 
+            this.s1.AutoSize = true;
+            this.s1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.s1.Location = new System.Drawing.Point(100, 158);
+            this.s1.Name = "s1";
+            this.s1.Size = new System.Drawing.Size(65, 20);
+            this.s1.TabIndex = 2;
+            this.s1.Text = "2";
+            // 
+            // s2
+            // 
+            this.s2.AutoSize = true;
+            this.s2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.s2.Location = new System.Drawing.Point(100, 198);
+            this.s2.Name = "s2";
+            this.s2.Size = new System.Drawing.Size(65, 20);
+            this.s2.TabIndex = 3;
+            this.s2.Text = "3";
+            // 
+            // s3
+            // 
+            this.s3.AutoSize = true;
+            this.s3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.s3.Location = new System.Drawing.Point(100, 232);
+            this.s3.Name = "s3";
+            this.s3.Size = new System.Drawing.Size(65, 20);
+            this.s3.TabIndex = 3;
+            this.s3.Text = "4";
+            // 
+            // s4
+            // 
+            this.s4.AutoSize = true;
+            this.s4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.s4.Location = new System.Drawing.Point(100, 252);
+            this.s4.Name = "s4";
+            this.s4.Size = new System.Drawing.Size(65, 20);
+            this.s4.TabIndex = 3;
+            this.s4.Text = "5";
+            // 
+            // s5
+            // 
+            this.s5.AutoSize = true;
+            this.s5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.s5.Location = new System.Drawing.Point(100, 272);
+            this.s5.Name = "s5";
+            this.s5.Size = new System.Drawing.Size(65, 20);
+            this.s5.TabIndex = 3;
+            this.s5.Text = "6";
+            // 
+            // s6
+            // 
+            this.s6.AutoSize = true;
+            this.s6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.s6.Location = new System.Drawing.Point(100, 292);
+            this.s6.Name = "s6";
+            this.s6.Size = new System.Drawing.Size(65, 20);
+            this.s6.TabIndex = 3;
+            this.s6.Text = "7";
+            // 
+            // s7
+            // 
+            this.s7.AutoSize = true;
+            this.s7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.s7.Location = new System.Drawing.Point(100, 312);
+            this.s7.Name = "s7";
+            this.s7.Size = new System.Drawing.Size(65, 20);
+            this.s7.TabIndex = 3;
+            this.s7.Text = "8";
+            // 
+            // s8
+            // 
+            this.s8.AutoSize = true;
+            this.s8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.s8.Location = new System.Drawing.Point(100, 332);
+            this.s8.Name = "s8";
+            this.s8.Size = new System.Drawing.Size(65, 20);
+            this.s8.TabIndex = 3;
+            this.s8.Text = "9";
+            // 
+            // s9
+            // 
+            this.s9.AutoSize = true;
+            this.s9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.s9.Location = new System.Drawing.Point(100, 352);
+            this.s9.Name = "s9";
+            this.s9.Size = new System.Drawing.Size(65, 20);
+            this.s9.TabIndex = 3;
+            this.s9.Text = "10";
             // 
             // ViewScoresForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(580, 427);
+            this.Controls.Add(this.scores);
+            this.Controls.Add(this.s0);
+            this.Controls.Add(this.s1);
+            this.Controls.Add(this.s2);
+            this.Controls.Add(this.s3);
+            this.Controls.Add(this.s4);
+            this.Controls.Add(this.s5);
+            this.Controls.Add(this.s6);
+            this.Controls.Add(this.s7);
+            this.Controls.Add(this.s8);
+            this.Controls.Add(this.s9);
             this.Name = "ViewScoresForm";
             this.Text = "ViewScoresForm";
             this.ResumeLayout(false);
@@ -56,6 +176,16 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label scores;
+        private System.Windows.Forms.Label s0;
+        private System.Windows.Forms.Label s1;
+        private System.Windows.Forms.Label s2;
+        private System.Windows.Forms.Label s3;
+        private System.Windows.Forms.Label s4;
+        private System.Windows.Forms.Label s5;
+        private System.Windows.Forms.Label s6;
+        private System.Windows.Forms.Label s7;
+        private System.Windows.Forms.Label s8;
+        private System.Windows.Forms.Label s9;
     }
 }
